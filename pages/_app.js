39ac6570448355239ede5a567dff6ddd14b9f4f1
JY00088 <dobisms@gmail.com>
2023-02-17 14:38:05 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
+//최종컴포넌트
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
---
+++ pages/src/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header>dod</header>
+      {children}
+      <footer>d-d</footer>
+    </>
+  );
+};
+
+export default Layout;
